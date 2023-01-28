240c57ed4651a9abd7313cccdf9f01f5f5f45e59
jamaluddinrumi <jamal.rumi@icloud.com>
2023-01-28 15:30:06 +0000

    add astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,14 @@ import storyblok from "@storyblok/astro";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
+
 const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  site: "https://www.indomascot.com",
   integrations: [
     storyblok({
       accessToken: env.STORYBLOK_TOKEN,
@@ -25,5 +28,6 @@ export default defineConfig({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
     compress(),
+    sitemap(),
   ],
 });
