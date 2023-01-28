2079c3bc2fe97964a46ab1f762e6c36c0c98b05c
jamaluddinrumi <jamal.rumi@icloud.com>
2023-01-28 05:56:54 +0000

    add astro node & pnpm packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import node from "@astrojs/node";
@@ -6,5 +6,7 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node()
+  adapter: node({
+    mode: "standalone",
+  }),
 });
