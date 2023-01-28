2fe96385f049b2dd8654a6c7cfd5bf1287983572
jamaluddinrumi <jamal.rumi@icloud.com>
2023-01-28 06:05:47 +0000

    remove astro node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-import node from "@astrojs/node";
-
-// https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: "static",
 });
