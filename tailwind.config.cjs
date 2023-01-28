7b1ee12cbd13495bd4f04c602fa6299a95a7f375
jamaluddinrumi <jamal.rumi@icloud.com>
2023-01-28 14:35:18 +0000

    import tailwind settings from indomascot.v2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,95 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      "indomascot-yellow": "#e59700",
+      "indomascot-blue": "#0033cc",
+      "indomascot-old-blue": "#003399",
+      "whatsapp-green": "#25d366",
+      "whatsapp-old-green": "#128c7e",
+      bca: "#4000a1",
+      mandiri: "#003066",
+      bni: "#f05921",
+      bri: "#00529c",
+      jenius: "#20a4dc",
+      white: colors.white,
+      black: colors.black,
+      slate: colors.slate,
+      gray: colors.gray,
+      zinc: colors.zinc,
+      neutral: colors.neutral,
+      stone: colors.stone,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      sky: colors.sky,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose,
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        serif: ["Josefin Sans", ...defaultTheme.fontFamily.serif],
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      },
+      animation: {
+        type: "type 2.7s ease-out .8s infinite alternate both",
+        text: "text 5s ease infinite",
+        "spin-slow": "spin 1.5s linear infinite",
+        "type-reverse": "type 1.8s ease-out 0s infinite alternate-reverse both",
+      },
+      keyframes: {
+        type: {
+          "0%": { transform: "translateX(0ch)" },
+          "5%, 10%": { transform: "translateX(1ch)" },
+          "15%, 20%": { transform: "translateX(2ch)" },
+          "25%, 30%": { transform: "translateX(3ch)" },
+          "35%, 40%": { transform: "translateX(4ch)" },
+          "45%, 50%": { transform: "translateX(5ch)" },
+          "55%, 60%": { transform: "translateX(7ch)" },
+          "65%, 70%": { transform: "translateX(9ch)" },
+          "75%, 80%": { transform: "translateX(12ch)" },
+          "85%, 90%": { transform: "translateX(14ch)" },
+          "95%, 100%": { transform: "translateX(15ch)" },
+        },
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+  ],
+};
