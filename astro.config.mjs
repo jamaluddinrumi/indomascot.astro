5775ba50671fbb96e0a8101590e31c09ee2b2827
jamaluddinrumi <jamal.rumi@icloud.com>
2023-01-28 14:52:24 +0000

    add astro storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,26 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
+import { loadEnv } from "vite";
+import storyblok from "@storyblok/astro";
 import tailwind from "@astrojs/tailwind";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  integrations: [tailwind()]
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        // Add your components here
+      },
+      apiOptions: {
+        // Choose your Storyblok space region
+        region: "eu", // optional,  or 'eu' (default)
+      },
+    }),
+    tailwind(),
+  ],
 });
