81530d97dd5718cf7a0ef05a59071e71639ce2b8
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-02-04 22:05:41 +0700

    build skeleton for pages

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { locale } from "@src/locale";
+import { useStore } from "@nanostores/vue";
+import LocalChanger from "@components/LocalChanger.vue";
+</script>
+
+<template>
+  <header
+    id="main-header"
+    class="header supports-backdrop-blur:bg-white/60 sticky top-0 z-[3001] w-full flex-none backdrop-blur transition"
+  >
+    <div class="grid-container">
+      <LocalChanger />
+    </div>
+  </header>
+</template>
