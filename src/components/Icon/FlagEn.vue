25cd759c5b1210b992369e3bda9cffbde83739fc
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-02-04 23:10:37 +0700

    add skeleton for base components

---
+++ src/components/Icon/FlagEn.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtImg
+    preload
+    provider="storyblok"
+    width="18"
+    height="16"
+    src="https://a.storyblok.com/f/118728/x/22fabb8ef9/gb.svg"
+    class="align-baseline shadow"
+    alt="bendera inggris"
+  />
+</template>
---
+++ src/components/Icon/FlagId.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtImg
+    preload
+    provider="storyblok"
+    width="18"
+    height="16"
+    src="https://a.storyblok.com/f/118728/x/8896bea3df/id.svg"
+    class="align-baseline shadow"
+    alt="bendera indonesia"
+  />
+</template>
