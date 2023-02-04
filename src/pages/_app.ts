09589fc74e681f0fe2dbf79859832455833708ea
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-02-04 22:06:19 +0700

    use alias for src/locales

--- src/pages/_app.ts
+++ src/pages/_app.ts
@@ -1,10 +1,10 @@
 import type { App } from "vue";
 
 import { createI18n } from "vue-i18n";
-import id from "../locales/id-ID";
-import en from "../locales/en-US";
+import id from "@locales/id-ID";
+import en from "@locales/en-US";
 
-const i18n = createI18n({
+export const i18n = createI18n({
   locale: "id",
   fallbackLocale: "en",
   legacy: true,
