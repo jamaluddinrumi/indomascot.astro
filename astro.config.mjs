a57634931aed75283141c9e33e90d4e1e2749435
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-02-09 07:25:47 +0000

    set i18n global feature flags

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,9 @@ export default defineConfig({
   vite: {
     logLevel: "info",
     define: {
-      __DATE__: `'${new Date().toISOString()}'`,
+      __VUE_I18N_FULL_INSTALL__: true,
+      __VUE_I18N_LEGACY_API__: false,
+      __INTLIFY_PROD_DEVTOOLS__: false,
     },
     server: {
       https:
