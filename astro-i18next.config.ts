731f36308ab7703f73227370357d8c47e822ac3d
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-02-18 20:37:00 +0000

    add pages for en routes

--- astro-i18next.config.ts
+++ astro-i18next.config.ts
@@ -6,6 +6,15 @@ const config: AstroI18nextConfig = {
   i18nextServer: {
     debug: true,
   },
+  routes: {
+    en: {
+      "badut-maskot": "portfolio",
+      "cara-pemesanan": "how-to-order",
+      "tentang-kami": "about-us",
+      "syarat-ketentuan": "terms-and-conditions",
+      pencarian: "search",
+    },
+  },
 };
 
 export default config;
