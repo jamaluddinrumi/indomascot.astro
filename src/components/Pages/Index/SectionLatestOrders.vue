f9950b04b7e7408e05e52bfa186da78a7996485a
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-02-28 09:34:05 +0000

    move coding portfolio button directly inside section latest orders

--- src/components/Pages/Index/SectionLatestOrders.vue
+++ src/components/Pages/Index/SectionLatestOrders.vue
@@ -12,9 +12,14 @@ import { useStoryblok } from "@storyblok/vue";
 import { Image } from "@unpic/vue";
 import { useI18n } from "vue-i18n";
 import $device from "@src/device";
-import PortfolioButton from "@components/PortfolioButton.vue";
+import Human from "@components/Icon/Human.vue";
+import { menus } from "@src/states";
 
 const { t } = useI18n();
+
+const portfolioLink = ref(
+  menus.get().find((item) => item.text === "portfolio")
+);
 
 const props = defineProps<{
   modelValue?: boolean;
@@ -143,7 +148,39 @@ const modules = ref([Navigation, Pagination, EffectCoverflow]);
     <div class="swiper-button-next right-[4%] -mt-16 lg:right-[2%]" />
     <div class="swiper-pagination" />
   </swiper>
-  <PortfolioButton />
+  <div class="mb-8 mt-4 grid grid-cols-1 lg:mt-12">
+    <div class="relative flex justify-center">
+      <a
+        class="bg-gradient flex cursor-pointer justify-center rounded-full bg-primary px-6 py-4 uppercase tracking-wide shadow-2xl"
+        :href="portfolioLink.href"
+        :aria-label="t(portfolioLink.text)"
+      >
+        <div class="fa-fw fa-layers">
+          <Human />
+        </div>
+        <span
+          class="ml-0 mr-0.5 text-xs font-bold uppercase leading-normal lg:ml-0.5 lg:text-base"
+        >
+          {{ $t("showMore") }}
+        </span>
+      </a>
+      <span
+        class="absolute left-[calc(50%_+_5.5rem)] top-0 -mt-1 -mr-1 flex h-5 w-5"
+        :class="[
+          locale === 'id'
+            ? 'lg:left-[calc(50%_+_7rem)]'
+            : 'lg:left-[calc(50%_+_4.5rem)]',
+        ]"
+      >
+        <span
+          class="absolute inline-flex h-full w-full animate-ping rounded-full bg-gradient-to-t from-[#23c770] to-lime-400 opacity-75"
+        />
+        <span
+          class="relative inline-flex h-5 w-5 rounded-full bg-gradient-radial from-[#45e992] to-[#23c770]"
+        />
+      </span>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
