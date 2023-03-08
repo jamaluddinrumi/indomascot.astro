9ccdddc9292d3c8f0cb86bc2c38ef26a949a8f31
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-03-08 22:24:39 +0000

    perf: wrap portfolio in one vue file

---
+++ src/components/Pages/Portfolio/InitPage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import SectionPortfolio from "@components/Pages/Portfolio/SectionPortfolio.vue";
+import SectionTitle from "@components/Pages/Portfolio/SectionTitle.vue";
+</script>
+
+<template>
+  <SectionTitle client:only="vue" />
+  <SectionPortfolio client:only="vue" />
+</template>
