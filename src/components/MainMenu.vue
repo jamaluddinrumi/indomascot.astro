46c23cab5fb69ad5ed4a5ed195285e16cff51488
Jamaluddin Rumi <jamal.rumi@icloud.com>
2023-03-13 17:53:01 +0000

    feat: animate on leave when theme is switched on main menu

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -9,6 +9,7 @@ import { useI18n } from "vue-i18n";
 import { getBrowserHeight, Dimension } from "@src/dimension";
 import { prependTrailingSlash } from "@src/utils";
 import type { Menu } from "@src/menu";
+import { useMotions } from "@vueuse/motion";
 
 const themeStorage = useStorage("vueuse-color-scheme", "auto");
 
@@ -87,113 +88,126 @@ onMounted(() => {
         "data-theme": computed(() => themeStorage.value),
       },
     });
+
+    mainMenu.set(false);
   };
 });
+
+const motions = useMotions();
 </script>
 
 <template>
-  <div
-    v-if="$mainMenu"
-    id="main-menu"
-    v-motion
-    :initial="{
-      y: -browserDimension.height,
-    }"
-    :enter="{
-      y: 0,
-      transition: {
-        type: 'spring',
-        stiffness: 250,
-        damping: 25,
-        mass: 0.5,
-        delay: 10,
-      },
-    }"
-    :leave="{
-      y: -browserDimension.height,
-    }"
-    class="fixed top-0 left-0 z-[9999] mx-auto h-screen w-screen max-w-none overflow-y-scroll backdrop-blur-3xl"
-    style="-webkit-transform: translate3d(0, 0, 0)"
-  >
-    <div class="relative">
-      <nav
-        class="grid min-h-screen place-content-center overflow-y-scroll pt-16 pb-40 lg:pb-20"
-      >
-        <ul id="menu-halaman" ref="menuHalaman" class="vertical justify menu">
-          <li v-for="menu in menusRef" :key="menu.text" class="my-0.5">
-            <a
-              rel="prefetch"
-              class="flex justify-center rounded-full py-4 px-8"
-              :class="{
-                'btn-shadow btn-gradient active':
-                  menu.href ===
-                  (url.pathname || prependTrailingSlash(url.pathname)),
-              }"
-              :href="menu.href"
-              :aria-label="menu.href === '/' ? t('homepage') : menu.text"
-            >
-              <span class="josefin-sans text-base font-bold uppercase">
-                {{ t(menu.text) }}
-              </span>
-            </a>
-          </li>
-          <li class="my-2">
-            <div class="form-control hover:bg-transparent">
-              <label class="label">
-                <span class="label-text">{{ t("chooseLanguage") }}:</span>
-              </label>
-              <select
-                v-model="inheritLocaleRef"
-                @change="switchLanguage"
-                class="select-bordered select w-full max-w-xs"
+  <Transition :css="false" @leave="(_, done) => motions.transition.leave(done)">
+    <div
+      v-if="$mainMenu"
+      id="main-menu"
+      v-motion="'transition'"
+      :initial="{
+        y: -browserDimension.height,
+      }"
+      :enter="{
+        y: 0,
+        transition: {
+          type: 'spring',
+          stiffness: 250,
+          damping: 25,
+          mass: 0.5,
+          delay: 0,
+        },
+      }"
+      :leave="{
+        y: -browserDimension.height,
+        transition: {
+          type: 'spring',
+          stiffness: 250,
+          damping: 25,
+          mass: 0.5,
+          delay: 1000,
+        },
+      }"
+      class="fixed top-0 left-0 z-[9999] mx-auto h-screen w-screen max-w-none overflow-y-scroll backdrop-blur-3xl"
+      style="-webkit-transform: translate3d(0, 0, 0)"
+    >
+      <div class="relative">
+        <nav
+          class="grid min-h-screen place-content-center overflow-y-scroll pt-16 pb-40 lg:pb-20"
+        >
+          <ul id="menu-halaman" ref="menuHalaman" class="vertical justify menu">
+            <li v-for="menu in menusRef" :key="menu.text" class="my-0.5">
+              <a
+                rel="prefetch"
+                class="flex justify-center rounded-full py-4 px-8"
+                :class="{
+                  'btn-shadow btn-gradient active':
+                    menu.href ===
+                    (url.pathname || prependTrailingSlash(url.pathname)),
+                }"
+                :href="menu.href"
+                :aria-label="menu.href === '/' ? t('homepage') : menu.text"
               >
-                <option
-                  v-for="availableLocale in availableLocales"
-                  :key="availableLocale"
-                  :value="availableLocale"
-                  :selected="
-                    availableLocale === inheritLocaleRef ? true : false
-                  "
+                <span class="josefin-sans text-base font-bold uppercase">
+                  {{ t(menu.text) }}
+                </span>
+              </a>
+            </li>
+            <li class="my-2">
+              <div class="form-control hover:bg-transparent">
+                <label class="label">
+                  <span class="label-text">{{ t("chooseLanguage") }}:</span>
+                </label>
+                <select
+                  v-model="inheritLocaleRef"
+                  @change="switchLanguage"
+                  class="select-bordered select w-full max-w-xs"
                 >
-                  {{ t(`language.${availableLocale}`) }}
-                </option>
-              </select>
-            </div>
-          </li>
-          <li class="my-2">
-            <div class="form-control hover:bg-transparent">
-              <label class="label">
-                <span class="label-text">{{ t("chooseTheme") }}:</span>
-              </label>
-              <select
-                v-model="theme"
-                @change="switchTheme"
-                class="select-bordered select w-full max-w-xs"
-              >
-                <option
-                  v-for="(themeItem, index) in themes"
-                  :key="index"
-                  :value="themeItem"
-                  :selected="themeItem === theme"
+                  <option
+                    v-for="availableLocale in availableLocales"
+                    :key="availableLocale"
+                    :value="availableLocale"
+                    :selected="
+                      availableLocale === inheritLocaleRef ? true : false
+                    "
+                  >
+                    {{ t(`language.${availableLocale}`) }}
+                  </option>
+                </select>
+              </div>
+            </li>
+            <li class="my-2">
+              <div class="form-control hover:bg-transparent">
+                <label class="label">
+                  <span class="label-text">{{ t("chooseTheme") }}:</span>
+                </label>
+                <select
+                  v-model="theme"
+                  @change="switchTheme"
+                  class="select-bordered select w-full max-w-xs"
                 >
-                  {{ t(themeItem === true ? "dark" : "light") }}
-                </option>
-              </select>
-            </div>
-          </li>
-          <li class="mx-auto mt-11 flex w-fit justify-center lg:hidden">
-            <InstallButton :disabled="$device.isDesktop" icon="apple" />
-          </li>
-          <li class="mx-auto mt-2 flex w-fit justify-center lg:hidden">
-            <InstallButton :disabled="$device.isDesktop" icon="android" />
-          </li>
-          <li class="mx-auto mt-2 mb-4 flex w-fit justify-center lg:hidden">
-            <InstallButton icon="chrome" />
-          </li>
-        </ul>
-      </nav>
+                  <option
+                    v-for="(themeItem, index) in themes"
+                    :key="index"
+                    :value="themeItem"
+                    :selected="themeItem === theme"
+                  >
+                    {{ t(themeItem === true ? "dark" : "light") }}
+                  </option>
+                </select>
+              </div>
+            </li>
+            <li class="mx-auto mt-11 flex w-fit justify-center lg:hidden">
+              <InstallButton :disabled="$device.isDesktop" icon="apple" />
+            </li>
+            <li class="mx-auto mt-2 flex w-fit justify-center lg:hidden">
+              <InstallButton :disabled="$device.isDesktop" icon="android" />
+            </li>
+            <li class="mx-auto mt-2 mb-4 flex w-fit justify-center lg:hidden">
+              <InstallButton icon="chrome" />
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
-  </div>
+  </Transition>
 </template>
 
 <style lang="scss" scoped>
